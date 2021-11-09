f6c0e83d70692cb4fa8e6b4f80588bc7730fcea4
NofathZ <nofath86@gmail.com>
2021-11-10 01:10:47 +0700

    input sampah done

--- src/components/form-input-sampah/Question.js
+++ src/components/form-input-sampah/Question.js
@@ -6,16 +6,30 @@ const Question = (props) => {
   const tipeSampah = ["a", "b"]
 
   const output = () => {
-    const nama = document.querySelector(`#nama-sampah${props.idx}`).value
+    // const nama = document.querySelector(`#nama-sampah${props.idx}`).value
+    const idSamp = document.querySelector(`#id-sampah${props.idx}`).value
     const jumlah = document.querySelector(`#jumlah-sampah${props.idx}`).value
-    props.handleInput({nama, kuantitas: jumlah}, props.idx)
+
+    var jenisField = document.getElementById(`id-sampah${props.idx}`);
+    var value = jenisField.options[jenisField.selectedIndex].value;
+
+    // props.handleInput({ nama, kuantitas: jumlah }, props.idx)
+    props.handleInput({ id: value, kuantitas: jumlah }, props.idx)
+
   }
 
   return (
     <div className="row-box-input">
       <span className="lead-seven list-item-input">
         {/* <input id="nama-sampah" className="input-sampah-column" type="text" onChange={(e) => setNama(e.target.value)} /> */}
-        <input id={`nama-sampah${props.idx}`} className="input-sampah-column" type="text" onChange={output} />
+        {/* <input id={`nama-sampah${props.idx}`} className="input-sampah-column" type="text" onChange={output} /> */}
+
+        <select id={`id-sampah${props.idx}`} onChange={output}>
+          <option value="select">Select</option>
+          <option value="Java">Java</option>
+          <option value="C++">C++</option>
+        </select>
+
       </span>
       <span className="lead-seven list-item-input">
         <input id={`jumlah-sampah${props.idx}`} className="input-sampah-column" type="number" onChange={output} />
