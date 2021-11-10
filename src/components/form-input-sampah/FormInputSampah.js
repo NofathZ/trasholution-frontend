72cfc6a1ea1de95e6f914847947bba803afdd69e
NofathZ <nofath86@gmail.com>
2021-11-10 09:24:10 +0700

    input sampah done

--- src/components/form-input-sampah/FormInputSampah.js
+++ src/components/form-input-sampah/FormInputSampah.js
@@ -3,28 +3,49 @@ import { ButtonValid } from '../button/ButtonValid'
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import Question from './Question'
+import API from '../../api/api'
 
 const FormInputSampah = () => {
 
   const [idx, setIdx] = useState(0)
   const [outputList, setOutputList] = useState([])
   const [inputList, setInputList] = useState([])
+  const [namaSampah, setNamaSampah] = useState([])
+
+  useEffect(async () => {
+    const semuaSampah = []
+    const { data } = await API.get('/api/daftar-sampah')
+
+    data.data.forEach(data => {
+      if (!semuaSampah.includes(data.semuaSampah)) {
+        semuaSampah.push(
+          {
+            id: data.id,
+            nama: data.nama
+          }
+        )
+      }
+    })
+
+    setNamaSampah(semuaSampah)
+  }, [])
 
   const handleInput = (val) => {
     outputList[idx] = val
   }
 
   const onAddBtnClick = event => {
-    setInputList(inputList.concat(<Question idx={idx} handleInput={(val) => handleInput(val, idx)} key={inputList.length} />));
+    setInputList(inputList.concat(<Question namaSampah={namaSampah} idx={idx} handleInput={(val) => handleInput(val, idx)} key={inputList.length} />));
     setIdx(idx + 1)
   };
 
-  const showOutput = () => {
+  const showOutput = (e) => {
+    e.preventDefault()
     console.log(outputList)
   }
 
   return (
-    <>
+    <form onSubmit={showOutput}>
       <div className="form-input-sampah-box main-color-bg">
         <div className="row-box-input">
           <span className="lead-seven list-item-input">Nama</span>
@@ -41,9 +62,9 @@ const FormInputSampah = () => {
         </button>
       </div>
       {/* <Link to={'/permintaan-diterima'} style={{ textDecoration: "none" }}> */}
-      <ButtonValid className="lead-five" style={{ marginTop: "28px" }} onClick={showOutput}>Jual</ButtonValid>
+      <ButtonValid className="lead-five" style={{ marginTop: "28px" }}>Jual</ButtonValid>
       {/* </Link> */}
-    </>
+    </form>
   )
 }
 
