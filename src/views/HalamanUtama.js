b828b55f597034be270dfbc701bccec92a13530a
NofathZ <nofath86@gmail.com>
2021-11-14 15:03:40 +0700

    trying resolve problem

--- src/views/HalamanUtama.js
+++ src/views/HalamanUtama.js
@@ -16,7 +16,7 @@ const HalamanUtama = () => {
 
   useEffect(async () => { // trying
     const token = localStorage.getItem('token')
-    const currentPenjualan = await API.get('/api/p/current-penjualan', {
+    const currentPenjualan = await API.get('/api/t/current-penjemputan', {
       headers: {
         "Authorization": `Bearer ${token}`
       }
