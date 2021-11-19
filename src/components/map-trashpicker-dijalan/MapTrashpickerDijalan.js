d4d79c1d7d1682415f81f9bce57df25fe7ea9043
NofathZ <nofath86@gmail.com>
2021-11-19 22:07:47 +0700

    location setState

--- src/components/map-trashpicker-dijalan/MapTrashpickerDijalan.js
+++ src/components/map-trashpicker-dijalan/MapTrashpickerDijalan.js
@@ -3,15 +3,15 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 import { useState, useEffect } from "react"
 
 const MapTrashpickerDijalan = (props) => {
-  const [positionPengguna, setPositionPengguna] = useState(null)
-  const [positionTrashpicker, setPositionTrashpicker] = useState(null)
+  const [positionPengguna, setPositionPengguna] = useState([props.dataLokasi.lat_pengguna, props.dataLokasi.long_pengguna])
+  const [positionTrashpicker, setPositionTrashpicker] = useState([props.dataLokasi.lat_trashpicker, props.dataLokasi.long_trashpicker])
 
-  useEffect(() => {
-    if (props.dataLokasi) {
-      setPositionPengguna([props.dataLokasi.lat_pengguna, props.dataLokasi.long_pengguna])
-      setPositionTrashpicker([props.dataLokasi.lat_trashpicker, props.dataLokasi.long_trashpicker])
-    }
-  }, [props.dataLokasi])
+  // useEffect(() => {
+  //   if (props.dataLokasi) {
+  //     setPositionPengguna([props.dataLokasi.lat_pengguna, props.dataLokasi.long_pengguna])
+  //     setPositionTrashpicker([props.dataLokasi.lat_trashpicker, props.dataLokasi.long_trashpicker])
+  //   }
+  // }, [props.dataLokasi])
 
   return (
     <div className="map-trashpicker-dijalan-box">
